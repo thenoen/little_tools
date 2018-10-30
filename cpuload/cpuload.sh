96f941625babd6e944de63dff9f20b4e8620babb
marek <kordikmarek@gmail.com>
2018-10-30 17:19:09 +0100

    added an utility to display average cpu load

---
+++ cpuload/cpuload.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+PREV_STATS_LOCATION=/tmp/cpuload.previousStatus
+
+
+sleepDurationSeconds=$1
+
+previousStats=$(cat $PREV_STATS_LOCATION)
+
+currentStats=$(cat /proc/stat)    
+echo $currentStats > $PREV_STATS_LOCATION
+
+
+cpus=$(echo "$currentStats" | grep -P 'cpu' | awk -F " " '{print $1}')
+
+
+function processInput {
+    currentLine=$(echo "$currentStats" | grep "$cpu ")
+    user=$(echo "$currentLine" | awk -F " " '{print $2}')
+    nice=$(echo "$currentLine" | awk -F " " '{print $3}')
+    system=$(echo "$currentLine" | awk -F " " '{print $4}')
+    idle=$(echo "$currentLine" | awk -F " " '{print $5}')
+    iowait=$(echo "$currentLine" | awk -F " " '{print $6}')
+    irq=$(echo "$currentLine" | awk -F " " '{print $7}')
+    softirq=$(echo "$currentLine" | awk -F " " '{print $8}')
+    steal=$(echo "$currentLine" | awk -F " " '{print $9}')
+    guest=$(echo "$currentLine" | awk -F " " '{print $10}')
+    guest_nice=$(echo "$currentLine" | awk -F " " '{print $11}')
+
+    previousLine=$(echo "$previousStats" | grep "$cpu ")
+    prevuser=$(echo "$previousLine" | awk -F " " '{print $2}')
+    prevnice=$(echo "$previousLine" | awk -F " " '{print $3}')
+    prevsystem=$(echo "$previousLine" | awk -F " " '{print $4}')
+    previdle=$(echo "$previousLine" | awk -F " " '{print $5}')
+    previowait=$(echo "$previousLine" | awk -F " " '{print $6}')
+    previrq=$(echo "$previousLine" | awk -F " " '{print $7}')
+    prevsoftirq=$(echo "$previousLine" | awk -F " " '{print $8}')
+    prevsteal=$(echo "$previousLine" | awk -F " " '{print $9}')
+    prevguest=$(echo "$previousLine" | awk -F " " '{print $10}')
+    prevguest_nice=$(echo "$previousLine" | awk -F " " '{print $11}')    
+
+    PrevIdle=$((previdle + previowait))
+    Idle=$((idle + iowait))
+
+    PrevNonIdle=$((prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal))
+    NonIdle=$((user + nice + system + irq + softirq + steal))
+
+    PrevTotal=$((PrevIdle + PrevNonIdle))
+    Total=$((Idle + NonIdle))
+
+    totald=$((Total - PrevTotal))
+    idled=$((Idle - PrevIdle))
+
+    CPU_Percentage=$(awk "BEGIN {printf \"%3.0f\n\", ($totald - $idled)/$totald*100}")
+
+    if [[ "$cpu" == "cpu" ]]; then
+        # echo "total "$CPU_Percentage
+        echo $CPU_Percentage
+    # else
+        # echo $cpu" "$CPU_Percentage
+    fi
+}
+
+
+for cpu in $cpus
+do
+    processInput
+done
